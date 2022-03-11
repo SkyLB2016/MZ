fb2b8500f8aded0957d1977f488e7794691d90dd
李彬 <1136096189@qq.com>
2022-03-11 18:44:38 +0800

    明心见性

--- uidesign/src/main/java/com/sky/uidesign/entity/TitleEntity.kt
+++
@@ -1,9 +0,0 @@
-package com.sky.uidesign.entity
-
-/**
- * Created by libin on 2020/05/09 10:19 AM Saturday.
- */
-data class TitleEntity(
-    var centerTitle: String,
-    var rightTitle: String? = null
-)
